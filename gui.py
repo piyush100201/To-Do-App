8da1c61c5e8e378a5a125d9dc2423d4d78d64ad3
Piyush Jain <piyushjain100201@gmail.com>
2024-12-05 11:08:15 +0530

    Change theme and add clock

--- gui.py
+++ gui.py
@@ -1,6 +1,12 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todo.tx"):
+    with open("todo.txt","w") as file:
+        pass
+
 
 sg.theme("Black")
 
