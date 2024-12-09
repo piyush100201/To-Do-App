88639b81bb0f40190ba8dc15e6826f0320f08b91
Piyush Jain <piyushjain100201@gmail.com>
2024-12-09 11:38:02 +0530

    Completing items

--- web.py
+++ web.py
@@ -1,22 +1,25 @@
 import streamlit as st
 import functions
 
+
+todos = functions.read_todos()
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
-    todos = functions.read_todos()
     todos.append(todo)
     functions.write_todos(todos)
-
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 
-st.checkbox("Buy grocery")
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key = todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-todos = functions.read_todos()
-
-for todo in todos:
-    st.checkbox(todo)
 
 st.text_input(label = "", placeholder = "Add a new todo...",
               on_change = add_todo,key = "new_todo")
